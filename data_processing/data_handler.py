df78009ed05447f52ceec6353a1d7816f37dc3ae
nikldev0 <nikhil.kanukuntla@protonmail.com>
2024-07-10 23:24:59 +0100

    add all methods

--- data_processing/data_handler.py
+++ data_processing/data_handler.py
@@ -177,3 +177,330 @@ def process_group_fixtures(api_client, fixtures, group_name):
         print(f"No fixtures found for {group_name}")
         return pd.DataFrame(columns=columns)
     
+
+def process_round_of_16_fixtures(api_client, fixtures):
+    
+    fixtures_data = []
+    
+    for fixture in fixtures:
+        if fixture['league']['round'] == "Round of 16":
+            fixture_id = fixture['fixture']['id']
+            home_team = fixture['teams']['home']['name']
+            away_team = fixture['teams']['away']['name']
+            home_team_id = fixture['teams']['home']['id']
+            away_team_id = fixture['teams']['away']['id']
+            
+            events = get_fixture_events(api_client, fixture_id)
+            
+            if fixture['fixture']['status']['long'] == "Not Started":
+                # If the match has not started, only add the fixture ID, home, and away team names
+                fixtures_data.append([
+                    fixture_id,
+                    home_team,
+                    away_team,
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+                ])
+            else:
+                home_goals = fixture['goals']['home'] if fixture['goals']['home'] is not None else 0
+                away_goals = fixture['goals']['away'] if fixture['goals']['away'] is not None else 0
+                
+                if fixture['teams']['home']['winner'] is None and fixture['teams']['away']['winner'] is None:
+                    result = "Draw"
+                elif fixture['teams']['home']['winner']:
+                    result = f"{home_team} Victory"
+                else:
+                    result = f"{away_team} Victory"
+                
+                # Check if the match was decided by penalties
+                victory_on_penalties = "TRUE" if fixture['fixture']['status']['short'] == "PEN" else "FALSE"
+                
+                # Fetch the fixture events for cards
+                home_yellow_cards, home_red_cards, away_yellow_cards, away_red_cards = process_card_events(events, home_team_id, away_team_id)
+                
+                # Fetch the hat trick events
+                home_hat_tricks, away_hat_tricks = process_hat_trick_events(events, home_team_id, away_team_id)
+                
+                # Fetch the own goal events
+                home_own_goals, away_own_goals = process_own_goal_events(events, home_team_id, away_team_id)
+                
+                fixtures_data.append([
+                    fixture_id,
+                    home_team,
+                    away_team,
+                    result,
+                    victory_on_penalties,
+                    home_goals,
+                    away_goals,
+                    away_goals,
+                    home_goals,
+                    home_yellow_cards,
+                    home_red_cards,
+                    away_yellow_cards,
+                    away_red_cards,
+                    home_hat_tricks,
+                    away_hat_tricks,
+                    home_own_goals,
+                    away_own_goals
+                ])
+    
+    columns = ["Fixture ID", "Home", "Away", "Result", "Victory on Penalties?", "Home Goals Scored", "Home Goals Conceded", "Away Goals Scored", "Away Goals Conceded", "Home Yellow Cards", "Home Red Cards", "Away Yellow Cards", "Away Red Cards", "Home Hat Tricks", "Away Hat Tricks", "Home Own Goals", "Away Own Goals"]
+    
+    df = pd.DataFrame(fixtures_data, columns=columns)
+    
+    # Replace NaN values with 0
+    df = df.fillna(0)
+    
+    if not df.empty:
+        return df
+    else:
+        print(f"No fixtures found for Round of 16")
+        return pd.DataFrame(columns=columns)
+    
+
+
+
+
+
+
+
+
+
+
+def process_quarter_final_fixtures(api_client, fixtures):
+    
+    fixtures_data = []
+    
+    for fixture in fixtures:
+        if fixture['league']['round'] == "Quarter-finals":
+            fixture_id = fixture['fixture']['id']
+            home_team = fixture['teams']['home']['name']
+            away_team = fixture['teams']['away']['name']
+            home_team_id = fixture['teams']['home']['id']
+            away_team_id = fixture['teams']['away']['id']
+            
+            events = get_fixture_events(api_client, fixture_id)
+            
+            if fixture['fixture']['status']['long'] == "Not Started":
+                # If the match has not started, only add the fixture ID, home, and away team names
+                fixtures_data.append([
+                    fixture_id,
+                    home_team,
+                    away_team,
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+                ])
+            else:
+                home_goals = fixture['goals']['home'] if fixture['goals']['home'] is not None else 0
+                away_goals = fixture['goals']['away'] if fixture['goals']['away'] is not None else 0
+                
+                if fixture['teams']['home']['winner'] is None and fixture['teams']['away']['winner'] is None:
+                    result = "Draw"
+                elif fixture['teams']['home']['winner']:
+                    result = f"{home_team} Victory"
+                else:
+                    result = f"{away_team} Victory"
+                
+                # Check if the match was decided by penalties
+                victory_on_penalties = "TRUE" if fixture['fixture']['status']['short'] == "PEN" else "FALSE"
+                
+                # Fetch the fixture events for cards
+                home_yellow_cards, home_red_cards, away_yellow_cards, away_red_cards = process_card_events(events, home_team_id, away_team_id)
+                
+                # Fetch the hat trick events
+                home_hat_tricks, away_hat_tricks = process_hat_trick_events(events, home_team_id, away_team_id)
+                
+                # Fetch the own goal events
+                home_own_goals, away_own_goals = process_own_goal_events(events, home_team_id, away_team_id)
+                
+                fixtures_data.append([
+                    fixture_id,
+                    home_team,
+                    away_team,
+                    result,
+                    victory_on_penalties,
+                    home_goals,
+                    away_goals,
+                    away_goals,
+                    home_goals,
+                    home_yellow_cards,
+                    home_red_cards,
+                    away_yellow_cards,
+                    away_red_cards,
+                    home_hat_tricks,
+                    away_hat_tricks,
+                    home_own_goals,
+                    away_own_goals
+                ])
+    
+    columns = ["Fixture ID", "Home", "Away", "Result", "Victory on Penalties?", "Home Goals Scored", "Home Goals Conceded", "Away Goals Scored", "Away Goals Conceded", "Home Yellow Cards", "Home Red Cards", "Away Yellow Cards", "Away Red Cards", "Home Hat Tricks", "Away Hat Tricks", "Home Own Goals", "Away Own Goals"]
+    
+    df = pd.DataFrame(fixtures_data, columns=columns)
+    
+    # Replace NaN values with 0
+    df = df.fillna(0)
+    
+    if not df.empty:
+        return df
+    else:
+        print(f"No fixtures found for Quarter-finals")
+        return pd.DataFrame(columns=columns)
+    
+
+
+
+def process_semi_final_fixtures(api_client, fixtures):
+
+    fixtures_data = []
+    
+    for fixture in fixtures:
+        if fixture['league']['round'] == "Semi-finals":
+            fixture_id = fixture['fixture']['id']
+            home_team = fixture['teams']['home']['name']
+            away_team = fixture['teams']['away']['name']
+            home_team_id = fixture['teams']['home']['id']
+            away_team_id = fixture['teams']['away']['id']
+            
+            events = get_fixture_events(api_client, fixture_id)
+            
+            if fixture['fixture']['status']['long'] == "Not Started":
+                # If the match has not started, only add the fixture ID, home, and away team names
+                fixtures_data.append([
+                    fixture_id,
+                    home_team,
+                    away_team,
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+                ])
+            else:
+                home_goals = fixture['goals']['home'] if fixture['goals']['home'] is not None else 0
+                away_goals = fixture['goals']['away'] if fixture['goals']['away'] is not None else 0
+                
+                if fixture['teams']['home']['winner'] is None and fixture['teams']['away']['winner'] is None:
+                    result = "Draw"
+                elif fixture['teams']['home']['winner']:
+                    result = f"{home_team} Victory"
+                else:
+                    result = f"{away_team} Victory"
+                
+                # Check if the match was decided by penalties
+                victory_on_penalties = "TRUE" if fixture['fixture']['status']['short'] == "PEN" else "FALSE"
+                
+                # Fetch the fixture events for cards
+                home_yellow_cards, home_red_cards, away_yellow_cards, away_red_cards = process_card_events(events, home_team_id, away_team_id)
+                
+                # Fetch the hat trick events
+                home_hat_tricks, away_hat_tricks = process_hat_trick_events(events, home_team_id, away_team_id)
+                
+                # Fetch the own goal events
+                home_own_goals, away_own_goals = process_own_goal_events(events, home_team_id, away_team_id)
+                
+                fixtures_data.append([
+                    fixture_id,
+                    home_team,
+                    away_team,
+                    result,
+                    victory_on_penalties,
+                    home_goals,
+                    away_goals,
+                    away_goals,
+                    home_goals,
+                    home_yellow_cards,
+                    home_red_cards,
+                    away_yellow_cards,
+                    away_red_cards,
+                    home_hat_tricks,
+                    away_hat_tricks,
+                    home_own_goals,
+                    away_own_goals
+                ])
+    
+    columns = ["Fixture ID", "Home", "Away", "Result", "Victory on Penalties?", "Home Goals Scored", "Home Goals Conceded", "Away Goals Scored", "Away Goals Conceded", "Home Yellow Cards", "Home Red Cards", "Away Yellow Cards", "Away Red Cards", "Home Hat Tricks", "Away Hat Tricks", "Home Own Goals", "Away Own Goals"]
+    
+    df = pd.DataFrame(fixtures_data, columns=columns)
+    
+    # Replace NaN values with 0
+    df = df.fillna(0)
+    
+    if not df.empty:
+        return df
+    else:
+        print(f"No fixtures found for Semi-finals")
+        return pd.DataFrame(columns=columns)
+    
+
+def process_final_fixture(api_client, fixtures):
+
+    fixtures_data = []
+    
+    for fixture in fixtures:
+        if fixture['league']['round'] == "Final":
+            fixture_id = fixture['fixture']['id']
+            home_team = fixture['teams']['home']['name']
+            away_team = fixture['teams']['away']['name']
+            home_team_id = fixture['teams']['home']['id']
+            away_team_id = fixture['teams']['away']['id']
+            
+            events = get_fixture_events(api_client, fixture_id)
+            
+            if fixture['fixture']['status']['long'] == "Not Started":
+                # If the match has not started, only add the fixture ID, home, and away team names
+                fixtures_data.append([
+                    fixture_id,
+                    home_team,
+                    away_team,
+                    "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+                ])
+            else:
+                home_goals = fixture['goals']['home'] if fixture['goals']['home'] is not None else 0
+                away_goals = fixture['goals']['away'] if fixture['goals']['away'] is not None else 0
+                
+                if fixture['teams']['home']['winner'] is None and fixture['teams']['away']['winner'] is None:
+                    result = "Draw"
+                elif fixture['teams']['home']['winner']:
+                    result = f"{home_team} Victory"
+                else:
+                    result = f"{away_team} Victory"
+                
+                # Check if the match was decided by penalties
+                victory_on_penalties = "TRUE" if fixture['fixture']['status']['short'] == "PEN" else "FALSE"
+                
+                # Fetch the fixture events for cards
+                home_yellow_cards, home_red_cards, away_yellow_cards, away_red_cards = process_card_events(events, home_team_id, away_team_id)
+                
+                # Fetch the hat trick events
+                home_hat_tricks, away_hat_tricks = process_hat_trick_events(events, home_team_id, away_team_id)
+                
+                # Fetch the own goal events
+                home_own_goals, away_own_goals = process_own_goal_events(events, home_team_id, away_team_id)
+                
+                fixtures_data.append([
+                    fixture_id,
+                    home_team,
+                    away_team,
+                    result,
+                    victory_on_penalties,
+                    home_goals,
+                    away_goals,
+                    away_goals,
+                    home_goals,
+                    home_yellow_cards,
+                    home_red_cards,
+                    away_yellow_cards,
+                    away_red_cards,
+                    home_hat_tricks,
+                    away_hat_tricks,
+                    home_own_goals,
+                    away_own_goals
+                ])
+    
+    columns = ["Fixture ID", "Home", "Away", "Result", "Victory on Penalties?", "Home Goals Scored", "Home Goals Conceded", "Away Goals Scored", "Away Goals Conceded", "Home Yellow Cards", "Home Red Cards", "Away Yellow Cards", "Away Red Cards", "Home Hat Tricks", "Away Hat Tricks", "Home Own Goals", "Away Own Goals"]
+    
+    df = pd.DataFrame(fixtures_data, columns=columns)
+    
+    # Replace NaN values with 0
+    df = df.fillna(0)
+    
+    if not df.empty:
+        return df
+    else:
+        print(f"No fixtures found for Final")
+        return pd.DataFrame(columns=columns)
+    
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 
 from api.api_client import ApiClient
-from data_processing.data_handler import extract_team_names, process_group_fixtures
+from data_processing.data_handler import extract_team_names, process_group_fixtures, process_round_of_16_fixtures, process_quarter_final_fixtures, process_semi_final_fixtures, process_final_fixture
 from google_sheets.google_sheets_client import initialize_google_sheets, update_google_sheet
 import pandas as pd
 
@@ -15,6 +15,52 @@ def update_sheet_with_group_data(api_client, response, group_name, sheet_name):
     if not group_df.empty:
         sheet = initialize_google_sheets("Euro 24 Sweepstake League Tracker", sheet_name)
         update_google_sheet(sheet, group_df)
+
+
+def update_sheet_with_round_of_16_data(api_client, response, sheet_name):
+    round_of_16_df = process_round_of_16_fixtures(api_client, response['response'])
+    
+    print(f"{sheet_name} Fixtures DataFrame:")
+    print(round_of_16_df)
+    
+    if not round_of_16_df.empty:
+        sheet = initialize_google_sheets("Euro 24 Sweepstake League Tracker", sheet_name)
+        update_google_sheet(sheet, round_of_16_df)
+
+
+def update_sheet_with_quarter_final_data(api_client, response, sheet_name):
+    quarter_final_df = process_quarter_final_fixtures(api_client, response['response'])
+    
+    print(f"{sheet_name} Fixtures DataFrame:")
+    print(quarter_final_df)
+    
+    if not quarter_final_df.empty:
+        sheet = initialize_google_sheets("Euro 24 Sweepstake League Tracker", sheet_name)
+        update_google_sheet(sheet, quarter_final_df)
+
+
+def update_sheet_with_semi_final_data(api_client, response, sheet_name):
+    semi_final_df = process_semi_final_fixtures(api_client, response['response'])
+    
+    print(f"{sheet_name} Fixtures DataFrame:")
+    print(semi_final_df)
+    
+    if not semi_final_df.empty:
+        sheet = initialize_google_sheets("Euro 24 Sweepstake League Tracker", sheet_name)
+        update_google_sheet(sheet, semi_final_df)
+
+
+def update_sheet_with_final_data(api_client, response, sheet_name):
+    final_df = process_final_fixture(api_client, response['response'])
+    
+    print(f"{sheet_name} Fixtures DataFrame:")
+    print(final_df)
+    
+    if not final_df.empty:
+        sheet = initialize_google_sheets("Euro 24 Sweepstake League Tracker", sheet_name)
+        update_google_sheet(sheet, final_df)
+
+
 
 
 
@@ -40,8 +86,18 @@ def main():
         # update_sheet_with_group_data(api_client, response, "Group B", "Group B")
         # update_sheet_with_group_data(api_client, response, "Group C", "Group C")
         # update_sheet_with_group_data(api_client, response, "Group D", "Group D")
-        update_sheet_with_group_data(api_client, response, "Group E", "Group E")
-        update_sheet_with_group_data(api_client, response, "Group F", "Group F")
+        # update_sheet_with_group_data(api_client, response, "Group E", "Group E")
+        # update_sheet_with_group_data(api_client, response, "Group F", "Group F")
+
+
+        # update_sheet_with_round_of_16_data(api_client, response, "Round of 16")
+
+        # update_sheet_with_quarter_final_data(api_client, response, "Quarter Finals")
+
+        # update_sheet_with_semi_final_data(api_client, response, "Semi Finals")
+
+        update_sheet_with_final_data(api_client, response, "Final")
+
 
     except Exception as e:
         print(f"An error occurred: {e}")
